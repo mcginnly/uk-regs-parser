79b3cb1309e8d0a2d3baedf79e09ca414a4056a8
mcginnly <mcginnly@yahoo.co.uk>
2020-01-29 12:06:21 +0000

    add visibility field to return list

---
+++ Fparadrop.php
@@ -0,0 +1,32 @@
+<?php
+/*This function returns a list of all paragraph numbers */
+
+//turn debugging on
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+//define function
+function Fparadrop (){
+
+  //use global database connection
+  global $mysqli;
+ 
+  //query the database and return a row array for all paras
+  $pdquery = "SELECT para FROM B1v12019 WHERE para != ''";
+  $pdresult = $mysqli->query($pdquery);
+  
+
+  while ($test = mysqli_fetch_array($pdresult)) {
+ 
+    //add a visibility flag
+    $test['vis'] = 0;
+
+    $pdrow[]=$test;
+    
+  }
+
+ return $pdrow;
+ $pdresult -> free_result();
+
+} /*end function*/
+ ?>
